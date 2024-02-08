6194a77bbbbb9522ecce3c0bb558310c80e65471
maddy00o7 <149822847+maddy00o7@users.noreply.github.com>
2024-02-08 20:49:18 +0530

    Update application_start.sh

--- deploy/application_start.sh
+++ deploy/application_start.sh
@@ -1,5 +1,22 @@
 #!/bin/bash
 
-cd /home/ubuntu/simple-nodejs-app
-pm2 delete server
-pm2 start server.js
+# Change to your application directory
+cd /root/simple-nodejs-app
+
+# Add the npm bin directory to the PATH
+export PATH=$PATH:/root/.npm-global/bin
+
+# Stop the existing PM2 process for 'server'
+/usr/local/bin/pm2 stop server
+
+# Ensure the process is stopped before proceeding
+/usr/local/bin/pm2 delete server
+
+# Start the application using PM2
+/usr/local/bin/pm2 start server.js
+
+# Additional commands or configurations if needed
+# ...
+
+# Ensure the script exits with a success status
+exit 0
